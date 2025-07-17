7bff80c3757c1612eeae78290933f886317cb790
ThuongLe <thuongle@gaion.kr>
2025-07-17 11:57:20 +0700

    Update index.js to listen on all network interfaces by specifying hostname '0.0.0.0' for improved accessibility.

--- node-js-app/index.js
+++ node-js-app/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const hostname = '0.0.0.0';
 
 app.get('/', (req, res) => {
   res.send('Hello from Node.js on EC2 using CodeDeploy! 🚀');
 });
 
-app.listen(port, () => {
-  console.log(`App is running on http://localhost:${port}`);
+app.listen(port, hostname, () => {
+  console.log(`App is running on http://${hostname}:${port}`);
 });
